22e58d8db9e1d7eab25f9c43968cceb369414df9
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-25 12:09:02 +0100

    Mini Project

---
+++ src/MiniBookStore/NotebookService.java
@@ -0,0 +1,28 @@
+package MiniBookStore;
+
+public class NotebookService implements ProductService{
+    @Override
+    public void processMenu() {
+
+    }
+
+    @Override
+    public void listProduct() {
+
+    }
+
+    @Override
+    public void addProduct() {
+
+    }
+
+    @Override
+    public void removeProduct() {
+
+    }
+
+    @Override
+    public void filterProduct(String filter) {
+
+    }
+}
