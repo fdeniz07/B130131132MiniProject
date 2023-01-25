9d5bd3c91e7c618e25608ca584a1d2fe1b36ea08
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-25 12:09:15 +0100

    Mini Project

---
+++ src/MiniBookStore/ProductService.java
@@ -0,0 +1,14 @@
+package MiniBookStore;
+
+public interface ProductService {
+
+    void processMenu();
+
+    void listProduct();
+
+    void addProduct();
+
+    void removeProduct();
+
+    void filterProduct(String filter);
+}
